fb331fbc9fce3bd8a633eecd8cbbd2c424257620
srai <srai@yahoo-inc.com>
2014-07-15 18:35:44 -0700

    Initial Commit

---
+++ parsertest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  parsertest
+//
+//  Created by Satyajit Rai on 7/15/14.
+//  Copyright (c) 2014 Yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
